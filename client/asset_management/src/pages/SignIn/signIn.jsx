8bc7e885e47237bfea7a771864507e0a21ac9452
codeher-rahavi <rahaviganeshan529@gmail.com>
2026-03-03 10:02:04 +0530

    added server setup and updated signIn

--- client/asset_management/src/pages/SignIn/signIn.jsx
+++ client/asset_management/src/pages/SignIn/signIn.jsx
@@ -7,21 +7,23 @@ import EmailInput from "../../components/input/email";
 
 const SignIn = () => {
     const navigate = useNavigate();
-    const [email,setEmail] = useState("");
 
     return (
-        <div className="grid grid-cols-[700px_1fr] h-screen">
-            
+        <div className="grid sm:grid-cols-1 lg:grid-cols-[660px_1fr] min-h-screen">
+
             <div className="relative">
                 <form onSubmit="">
                     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-4 w-[400px]">
                         <h3 className="text-3xl mb-6 font-semibold">Log In</h3>
-                        <EmailInput/>
-                        <PassWord/>
+                        <EmailInput />
+                        <PassWord />
 
-                        <a class="inline-block rounded-sm bg-indigo-600 px-8 py-3 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl text-center" href="#">
+                        <button
+                            type="submit"
+                            className="inline-block rounded-sm bg-indigo-600 px-8 py-3 text-sm font-medium text-white transition hover:scale-110 hover:shadow-xl text-center"
+                        >
                             Log In
-                        </a>
+                        </button>
                         <p className="text-blue-700 flex justify-end">Forget password?</p>
                         <div className="flex items-center gap-4">
                             <div className="h-px bg-gray-400 flex-1"></div>
@@ -33,14 +35,14 @@ const SignIn = () => {
                             <button type="submit" className="border h-[50px] w-[200px] font-medium  rounded-md border-gray-400 cursor-pointer text-[18px] hover:shadow-xl hover:scale-95 ml-[100px]">
                                 <img src="/google.svg" alt="google-logo" className="w-7 inline mr-2" />
                                 Google
-                        </button>
-                            
+                            </button>
+
                         </div>
                         <p className="mt-4">Don't have an account ? <span className="text-blue-700 cursor-pointer underline" onClick={() => navigate("/SignUp")}>Sign Up</span></p>
                     </div>
                 </form>
             </div>
-            <div className="">
+            <div className="relative">
                 <div className="absolute z-10  top-[100px]  text-2xl w-[610px] ml-24">
                     <p className="text-3xl leading-12">The Future belongs to those who <span className="text-blue-600">believe</span> in the <span className="text-blue-600">beauty of theirs of Dreams..</span> </p>
                     <p className="text-xl ml-98 mt-2 font-semibold"> -Eleanor Roosevelt</p>
