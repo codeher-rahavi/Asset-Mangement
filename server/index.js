2d1e634c13886f20838600e6deb1e4f86fd1fca8
codeher-rahavi <rahaviganeshan529@gmail.com>
2026-03-04 14:54:30 +0530

    dotenv  installation

--- server/index.js
+++ server/index.js
@@ -1,19 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose"); // Ensure mongoose is required
 const authController = require("./controllers/authController");
-
+const { protect } = require("./middleware/authMiddleware");
 const cookieParser = require('cookie-parser');
+
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({
+  origin: "http://localhost:5173", // Replace with her actual frontend URL
+  credentials: true // MANDATORY: Allows the browser to send/receive cookies
+})); 
 app.use(cookieParser());
 
 
 // REPLACE THIS WITH YOUR ACTUAL CONNECTION STRING
-const mongoURI = "mongodb+srv://rahavi-ganeshan:rahavi_user@cluster0.2x3aoq.mongodb.net/?appName=Cluster0"; 
-
+const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI)
   .then(() => console.log("✅ MongoDB Connected..."))
   .catch(err => console.error("❌ MongoDB Connection Error:", err));
@@ -21,5 +25,15 @@ mongoose.connect(mongoURI)
 app.post("/api/check-email", authController.checkEmailAvailability);
 app.post("/api/signup", authController.signup);
 app.post("/api/login", authController.login);
+
+app.get("/api/overview", protect, (req, res) => {
+  res.status(200).json({
+    message: "Authorized access granted",
+    user: {
+      email: req.user.email,
+      id: req.user._id
+    }
+  });
+});
 
 app.listen(8000, () => console.log("Server running on port 8000"));
